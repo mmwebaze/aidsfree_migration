e3836466dc78b517ab15de66962ae5b14aa8fe7b
Michael Mwebaze <michael.mwebaze@gmail.com>
2017-08-07 09:51:14 -0400

    initial commit

---
+++ src/Plugin/migrate/process/NodeBodyImport.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\aidsfree_migration\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Node body manipulation from the d7 database
+ *
+ * @MigrateProcessPlugin(
+ *   id = "node_body"
+ * )
+ *
+ */
+class NodeBodyImport extends ProcessPluginBase {
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property){
+    return $value;
+  }
+}
---
+++ src/Plugin/migrate/source/AidsfreeUser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\aidsfree_migration\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+
+/**
+ * Aidsfree user from the d7 database
+ *
+ * @MigrateSource(
+ *   id = "aidsfree_user_sql"
+ * )
+ *
+ */
+class AidsfreeUser extends SqlBase {
+
+  public function fields() {
+    $fields = [
+      'uid' => $this->t('User ID'),
+      'name' => $this->t('Unique username'),
+      'pass' => $this->t('User password hashed'),
+      'mail' => $this->t('User email address'),
+      'created' => $this->t('Timestamp for when user was created'),
+      'access' => $this->t('Timestamp for previous time user accessed the site'),
+      'login' => $this->t('Timestamp for user last login'),
+      'status' => $this->t('Whether the user is active(1) or blocked(0)'),
+      'timezone' => $this->t('User timezone'),
+      'init' => $this->t('E-mail address used for initial account creation.'),
+    ];
+
+    return $fields;
+  }
+
+  public function getIds() {
+    return [
+      'uid' => [
+        'type' => 'integer'
+      ]
+    ];
+  }
+
+  public function query() {
+    $query =  $this->select('users', 'u')
+      ->fields('u', array_keys($this->fields()))
+      ->condition('uid', 1, '>');
+
+    return $query;
+  }
+}
